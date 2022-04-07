a91f073204a84d27ee3dd2d90846dac8ab7ec340
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-04-06 23:21:57 -0500

    commit de prueba en branch main

---
+++ imagenes/binarizacion.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+umbral = int(input('valor del Umbral'))
+print("este commit pertenece al branch 1")
+img = cv2.imread('rose1.jpg', 0)
+cv2.imshow("imagen original", img)
+largo, alto = img.shape
+
+
+img2 = np.zeros(img.shape, img.dtype)
+
+for x in range(largo):
+    for y in range(alto):
+        if(img[x, y]<umbral):
+            img2[x, y] = 0
+        else:
+            img2[x,y] = 255
+
+
+cv2.imshow("imagen umnbralada", img2)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
