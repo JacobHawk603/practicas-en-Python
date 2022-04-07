91b0eb9e3d4cf5d0ea50c6686af2ee6f36d5674f
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-04-06 23:57:30 -0500

    segunda prueba de branches

--- imagenes/binarizacion.py
+++ imagenes/binarizacion.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 
 umbral = int(input('valor del Umbral'))
-print("este commit pertenece al branch 1")
+print("este commit pertenece al branch 1 y modificamos en una segunda prueba")
+print("este otro comentario tambien pertenece al branch main")
 img = cv2.imread('rose1.jpg', 0)
 cv2.imshow("imagen original", img)
 largo, alto = img.shape
