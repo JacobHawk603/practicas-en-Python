adacc4c1ec92df11a992c1894c5e04e6906a866a
Jacobhawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-08-28 21:42:23 -0500

    subiendo mas practicas al repo

---
+++ Lenguaje natural/expresiones regulares/prectica1.py
@@ -0,0 +1,42 @@
+import re
+
+
+def listToString(lista):
+    cadena = ''
+    for i in range(len(lista)):
+        cadena += lista[i] +'\t'
+    return cadena
+
+archivo = open('tweets.txt', 'r', encoding='utf8')
+
+dataset = archivo.read()
+
+#print(dataset)
+#outStream = open("busqueda.txt", "w")
+
+cadenaPrueba = "mi cadena de #prueba aidiconal \u0144 13:45 2:10 2:01 21/01/2002 1945/17/09 10/02/21 050502/08/12226 :D :d :p :) :v :V xd XD xD :3"
+
+#print(re.findall("#+\w{1,30}", cadenaPrueba))
+#print(re.findall("\w{1,100}[^\u0144]", cadenaPrueba))
+#print(re.findall("\d{1,2}:\d{1,2}", cadenaPrueba))
+#print(re.findall("\d{2,4}/\d{1,2}/\d{2,4}", cadenaPrueba))
+#print(re.findall("[:,x,X][D,d,p,v,V,)]", cadenaPrueba))
+
+#print(listToString(re.findall("#+\w{1,30}", dataset)))
+#print(len(re.findall("#+\w{1,30}", dataset)))
+
+#print(listToString(re.findall("@+\w{1,30}", dataset)))
+#print(len(re.findall("@+\w{1,30}", dataset)))
+
+#print(listToString(re.findall("\d{1,2}:\d{1,2}", dataset)))
+#print(len(re.findall("\d{1,2}:\d{1,2}", dataset)))
+
+#print(listToString(re.findall("\d{2,4}/\d{1,2}/\d{2,4}", dataset)))
+#print(len(re.findall("\d{2,4}/\d{1,2}/\d{2,4}", dataset)))
+
+print(listToString(re.findall("[:,x,X][D,d,p,v,V,)]", dataset)))
+print(len(re.findall("[:,;,x,X][D,d,p,v,V,/)]", dataset)))
+
+#outStream.write(listToString(re.findall("#+\w{1,30}[^\u0144,\U0001f4a1,\u2661,\U0001f308,\U0001f62b,\U0001f60c,\U0001f339,\U0001f635,\U0001f499]", dataset)))
+#print("\u0144")
+#print("\U0001f308")
--- numeros primos/contadorPrimos.py
+++ numeros primos/contadorPrimos.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 primos = []
-primosCad = []
+primosBin = []
+arregloContadoresBinarios = []
 m = int(input("inserte el numero maximo de primos: "))
 
 def encontrarPrimo(primos, numero):
@@ -20,6 +22,16 @@ def convertirACadena(primos):
     print(cadenaArr)
     return cadenaArr
 
+def contarUnos(arregloPrimosBinario):
+    arregloContadores = []
+    for i in range(len(arregloPrimosBinario)):
+        contador = 0
+        for j in range(len(arregloPrimosBinario[i])):
+            if arregloPrimosBinario[i][j] == '1':
+                contador += 1
+        arregloContadores.append(contador)
+    return arregloContadores
+
 i = 2
 while(len(primos) < m):
 
@@ -29,4 +41,27 @@ while(len(primos) < m):
     
     i+=1
 print(primos)
-primosCad = convertirACadena(primos)
+primosBin = convertirACadena(primos)
+arregloContadoresBinarios = contarUnos(primosBin)
+print(arregloContadoresBinarios)
+
+#ploteo de la grafica---------------------------------------------------------------------------------------------------
+
+# make the data
+np.random.seed(3)
+x = 4 + np.random.normal(0, 2, 24)
+y = 4 + np.random.normal(0, 2, len(x))
+
+# size and color:
+sizes = np.random.uniform(15, 80, len(primos))
+colors = np.random.uniform(15, 80, len(arregloContadoresBinarios))
+
+# plot
+fig, ax = plt.subplots()
+
+ax.scatter(primos, arregloContadoresBinarios, s=sizes, c=colors, vmin=0, vmax=100)
+
+ax.set(xlim=(0, 8), xticks=primos,
+       ylim=(0, 8), yticks=np.arange(1, 20))
+
+plt.show()
