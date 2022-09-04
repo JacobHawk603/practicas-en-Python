9e0ca37b21a54cf24a1d2f342babc968995c07f5
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-09-04 18:12:37 -0500

    practica de manejo de archivos al 90 por ciento

---
+++ Lenguaje natural/manejo de archivos/main.py
@@ -0,0 +1,32 @@
+import spacy 
+
+nlp = spacy.load("es_core_news_sm")
+archivo_entrada = open('archivo_ejercicio_entrada.txt', encoding='utf8')
+
+dataset = archivo_entrada.readlines()
+print(dataset)
+archivo_entrada.close()
+
+normalizado = ""
+
+for lines in dataset:
+    if(len(lines)>1):
+        lines.replace('\n', '')
+        doc = nlp(lines)
+
+        for token in doc:
+            print(token.text, token.pos_, token.dep_, token.lemma_)
+            normalizado += token.lemma_.strip() + " "
+        print(lines)
+    else:
+        normalizado += "\n"
+    
+
+print(doc)
+print("\n\n")
+
+print(normalizado)
+
+archivo_salida = open('archivo_ejercicio_salida.txt', 'w')
+archivo_salida.write(normalizado)
+archivo_salida.close()
