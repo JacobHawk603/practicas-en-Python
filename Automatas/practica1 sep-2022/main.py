0b55e123dada8dec5141dc9a8dbf1e5338bb4859
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-10-23 23:21:52 -0500

    anadiendo modo grafco a los automatas

--- Automatas/practica1 sep-2022/main.py
+++ Automatas/practica1 sep-2022/main.py
@@ -1,7 +1,9 @@
+from asyncore import write
 from pickle import FALSE, TRUE
 from pprint import pprint
 from random import randint
 import automataParesCerosUnos as filtroPares
+import turtle
 
 
 def generadorDeCadenas64Bits(cantidad):
@@ -70,8 +72,98 @@ def sendingState(archivo):
 
 
 def main():
-    while(readyState(randint(0,1))):
-        print("Protocolo completado")
+
+    #No te olvides de descomentar esta parte para que funcione correctamenete el programa
+    '''while(readyState(randint(0,1))):
+        print("Protocolo completado")'''
+
+    #a partir de este punto comenzamos a programar el modo grafico-------------------------------------------------------
+
+    #coordenadas de mi tortuga
+    x = 0
+    y = 0
+
+    ventana = turtle.Screen()
+    ventana.bgcolor("light blue")
+    ventana.title("protocolo")
+
+    tortuga = turtle.Turtle()
+    tortuga.penup()
+
+    x = -250
+    y = 80
+
+    tortuga.setx(x)
+    tortuga.pendown()
+    tortuga.circle(80)
+    tortuga.penup()
+
+    x -= 50
+
+    tortuga.goto(x,y)
+    tortuga.write("ReadyState", font=("Arial", 18, "normal"))
+
+    x +=130
+
+    tortuga.goto(x, y)
+    #tortuga.penup()
+    tortuga.pendown()
+    tortuga.forward(317)
+
+
+    tortuga.fillcolor(0,0,0)
+    tortuga.begin_fill()
+    tortuga.circle(3)
+    tortuga.end_fill()
+
+    tortuga.forward(3)
+
+    x+=400
+    y=0
+
+    tortuga.penup()
+    tortuga.goto(x, y)
+    tortuga.pendown()
+    tortuga.circle(80)
+    tortuga.penup()
+
+    x -=50
+    y=50
+
+    tortuga.goto(x,y)
+    tortuga.write("Sending\nState", font=("Arial", 18, "normal"))
+
+    x +=130
+    y = 80
+
+    tortuga.penup()
+    tortuga.goto(x, y)
+    tortuga.pendown()
+    tortuga.circle(80, 270)
+
+    tortuga.begin_fill()
+    tortuga.circle(3)
+    tortuga.end_fill()
+
+    x = -250
+    y = 0
+
+    tortuga.penup()
+    tortuga.goto(x, y)
+    tortuga.pendown()
+
+    tortuga.begin_fill()
+    tortuga.circle(3)
+    tortuga.end_fill()
+
+    tortuga.circle(250, 180)
+    tortuga.penup()
+
+    tortuga.goto(0,0)
+
+    turtle.done()
+
+    #--------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     main()
