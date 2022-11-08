997adbfe1cb0d5fb3ed2c019bf1236f9cf99d62f
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-11-07 18:22:36 -0600

    que despapaye

--- conjunto potencia/potencia.py
+++ conjunto potencia/potencia.py
@@ -1,74 +1,70 @@
 import numpy as np
 import vispy.plot as vp
 import librerias.contadorPrimosLib as contador
+import math
 
 from asyncore import write
 
-token = open("./token.txt", "r").read()
+def main():
+    archivo = "./listaUnos.txt"
 
-if len(token) == 0:
-    token = open("./token.txt", "w")
-    token.write('0')
-    token.close()
     token = open("./token.txt", "r").read()
 
-print(token)
-k = int(token)
-#token.close()
+    if len(token) == 0:
+        token = open("./token.txt", "w")
+        token.write('0')
+        token.close()
+        token = open("./token.txt", "r").read()
 
-if k >= 20:
-    #Aquí se puede poner el algoritmo para plotear con vispy los unos totales.
+    print(token)
+    k = int(token)
+    #token.close()
 
-    listaUnos = open("./listaUnos.txt", "r")
-    arregloContadores = listaUnos.read().split(",")
+    if k >= 23:
+        graficar(archivo)
 
-    for i in range(len((arregloContadores))-1):
-        arregloContadores[i] = int(arregloContadores[i])
 
-    arregloContadores.pop()
-    #hora de realizar el plot--------------------------------------------------------
+        cadenas64Bits("./conjuntoPotencia.txt")
+        graficar("./listaUnos64.txt")
 
-    color = (0.3, 0.5, 0.8)
-    n_bins = 100
+        logaritmo("./unos64.txt")
+        graficar("./logaritmo.txt")
 
-    fig = vp.Fig(show=False)
-    line = fig[0:4, 0:4].plot(arregloContadores, symbol='o', width=0,
-                            face_color=color + (1,), edge_color=None,
-                            marker_size=4)
-    line.set_gl_state(depth_test=False)
-    fig.show(run=True)
+        exit()
 
-    #fin del plot--------------------------------------------------------------------
-    print("se ha alcanzo el total de elementos que queremos del conjunto potencia")
-    exit()
-
-else:
-    m = int(input("ingrese el valor de m: "))
-    #k = int(input("ingrese el valor de k: "))
-
-    if k == 0:
-        cadena = '{e},{0,1},'
     else:
-        cadena = ""
-    cadenaBin = ''
-    for i in range(k, m-1):
-        cadena +='{'
-        for j in range(2**(i+2)):
-            cadenaBin = '' + bin(j)[2:]
-            #print(bin(j)[2:])
-            aux = '#0' + str(i+4) + 'b'
-            cadena+= format(j, aux)[2:] + ','
-        cadena+= '},'
+        
+        m = int(input("ingrese el valor de m: "))
+        #k = int(input("ingrese el valor de k: "))
 
-    #print(cadena)
-    token = open("./token.txt", "w")
-    token.write(str(m-1))
-    token.close()
+        if k == 0:
+            cadena = '{e},{0,1},'
+        else:
+            cadena = ""
+        cadenaBin = ''
+        for i in range(k, m-1):
+            cadena +='{'
+            for j in range(2**(i+2)):
+                cadenaBin = '' + bin(j)[2:]
+                #print(bin(j)[2:])
+                aux = '#0' + str(i+4) + 'b'
+                cadena+= format(j, aux)[2:] + ','
+            cadena+= '},'
+
+        #print(cadena)
+        token = open("./token.txt", "w")
+        token.write(str(m-1))
+        token.close()
 
 
-    txt = open("./conjuntoPotencia.txt",'a')
-    txt.write(cadena)
-    txt.close()
+        txt = open("./conjuntoPotencia.txt",'a')
+        txt.write(cadena)
+        txt.close()
+
+        #aqui
+        crearArchivoDeunos(cadena, "./listaUnos.txt")
+
+def crearArchivoDeunos(cadena, archivoDestino):
 
     #Creamos el archivo txt que contiene la información de los unos que tiene cada elemento del conjunto potencia
 
@@ -85,6 +81,93 @@ else:
         
         cadenaContadores += str(arregloContadores[i]) + ","
 
-    listaUnos = open("./listaUnos.txt", "a")
+    listaUnos = open(archivoDestino, "a")
     listaUnos.write(cadenaContadores)
     listaUnos.close()
+
+def graficar(archivo):
+    #Aquí se puede poner el algoritmo para plotear con vispy los unos totales.
+
+    listaUnos = open(archivo, "r")
+    arregloContadores = listaUnos.read().split(",")
+
+    for i in range(len((arregloContadores))-1):
+        arregloContadores[i] = float(arregloContadores[i])
+
+    arregloContadores.pop()
+    #hora de realizar el plot--------------------------------------------------------
+
+    color = (0.3, 0.5, 0.8)
+    n_bins = 100
+
+    fig = vp.Fig(show=False)
+    line = fig[0:4, 0:4].plot(arregloContadores, symbol='o', width=0,
+                            face_color=color + (1,), edge_color=None,
+                            marker_size=4)
+    line.set_gl_state(depth_test=False)
+    fig.show(run=True)
+
+    #fin del plot--------------------------------------------------------------------
+    print("se ha alcanzado el total de elementos que queremos del conjunto potencia")
+
+def cadenas64Bits(archivo):
+    dataset = open(archivo, "r")
+
+    cadena = dataset.read()
+    dataset.close()
+
+    cadena = cadena.replace("}", "")
+    cadena = cadena.replace("{", "")
+    cadena = cadena.replace(",", "")
+    cadena = cadena.replace("e", "")
+
+    nuevaCadena = ""
+
+    try:
+        for i in range(0,len(cadena), 64):
+            for j in range(i, i+64):
+
+                nuevaCadena += cadena[j]
+            
+            nuevaCadena += ","
+
+    except:
+        nuevaCadena = nuevaCadena
+
+    crearArchivoDeunos(nuevaCadena, "./listaUnos64.txt")
+
+    nuevoArchivo = open("./unos64.txt", "w")
+    nuevoArchivo.write(nuevaCadena)
+    nuevoArchivo.close()
+
+def logaritmo(archivo):
+
+    dataset = open(archivo, "r")
+
+    datos = dataset.read()
+    dataset.close()
+
+    nuevCadena = ""
+
+    datosArr = datos.split(",")
+
+    for element in datosArr:
+        nuevCadena += str(math.log10(int(element)))
+        nuevCadena +=","
+
+
+    crearArchivoDeunos(nuevCadena, "./listaLogaritmo64.txt")
+
+    nuevoArchivo = open("logaritmo.txt", "w")
+    nuevoArchivo.write(nuevCadena)
+    nuevoArchivo.close
+    
+
+
+if __name__ == "__main__":
+
+    bandera = "y"
+
+    while(bandera == "y"):
+        main()
+        bandera = input("calcular otro valor de n? [n/y]")
--- numeros primos/contadorPrimos.py
+++ numeros primos/contadorPrimos.py
@@ -1,17 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import vispy.plot as vp
+import math
 
-
-primos = []
-primosBin = []
-arregloContadoresBinarios = []
-m = int(input("calcular primos hasta: "))
-k = 0
-cadenaContadores = ""
-cadenaBinarios = ""
-cadenaPrimos = ""
-cantidadPrimos = 0
 
 def encontrarPrimo(primos, numero):
     bandera = True
@@ -43,110 +34,198 @@ def contarUnos(arregloPrimosBinario):
         arregloContadores.append(contador)
     return arregloContadores
 
-#creamos el token para facilitar el trabajo a la maquina
+def main():
 
-try:
-    token = open("./token.txt", "r")
-    tokens = token.read().split(",")
-    k = int(tokens[0])
-    cantidadPrimos = int(tokens[1])
+    primos = []
+    primosBin = []
+    arregloContadoresBinarios = []
+    m = int(input("calcular primos hasta: "))
+    k = 0
+    cadenaContadores = ""
+    cadenaBinarios = ""
+    cadenaPrimos = ""
+    cantidadPrimos = 0
+
+    #creamos el token para facilitar el trabajo a la maquina
+
+    try:
+        token = open("./token.txt", "r")
+        tokens = token.read().split(",")
+        k = int(tokens[0])
+        cantidadPrimos = int(tokens[1])
+        token.close()
+    except:
+        k = 2
+        print("no hay token, k = 2")
+
+    #creamos el txt con todos los primos que hemos calculado hasta el momento
+
+    try:
+        primos = open("./primos.txt", "r").read().split(",")
+        primos.pop()
+
+        for i in range(len(primos)):
+            primos[i] = int(primos[i])    
+
+        print("el arreglo de primos no esta vacio")
+    except:
+        print("el arreglo de numeros primos comienza vacío")
+
+    i = k
+    while(i < m):
+
+        if encontrarPrimo(primos, i):
+            primos.append(i)
+            #print(primos)
+        
+        if i%2==0:
+            i+=1
+        else:
+            i+=2
+    #print(primos)
+    primosBin = convertirACadena(primos)
+    arregloContadoresBinarios = contarUnos(primosBin)
+    #print(arregloContadoresBinarios)
+
+    #guardamos los arreglos con los elementos que ya tenemos guardados
+    token = open("./token.txt", "w")
+    token.write(str(i) + "," + str(len(primos))) #anteriormente le dimos a i el valor de k que es el token inicial, así que i es el token final
     token.close()
-except:
-    k = 2
-    print("no hay token, k = 2")
 
-#creamos el txt con todos los primos que hemos calculado hasta el momento
+    for j in range(cantidadPrimos, len(primos)): #Tenemos que corregir esta parte, esto queda pendiente
+        cadenaPrimos += str(primos[j]) + ","
 
-try:
-    primos = open("./primos.txt", "r").read().split(",")
-    primos.pop()
+    archivoPrimos = open("./primos.txt", "a")
+    archivoPrimos.write(cadenaPrimos)
+    archivoPrimos.close()
 
-    for i in range(len(primos)):
-        primos[i] = int(primos[i])    
+    for j in range (cantidadPrimos, len(primos)):
+        cadenaBinarios += primosBin[j] + ","
 
-    print("el arreglo de primos no esta vacio")
-except:
-    print("el arreglo de numeros primos comienza vacío")
+    archivoPrimosBinario = open("./primosBin.txt", "a")
+    archivoPrimosBinario.write(cadenaBinarios)
+    archivoPrimosBinario.close()
 
-i = k
-while(i < m):
+    for j in range (cantidadPrimos, len(primos)):
+        cadenaContadores += str(arregloContadoresBinarios[j]) + ","
 
-    if encontrarPrimo(primos, i):
-        primos.append(i)
-        #print(primos)
+    archivoContadores = open("./contadoresPrimos.txt", "a")
+    archivoContadores.write(cadenaContadores)
+    archivoContadores.close()
+
+
+
+    #antes de plotear, obtenemos el total de los primos que tenemos en el archivo-------------------------------------------
+
+    #ploteo de la grafica
+
+    graficar("./contadoresPrimos.txt")
+
+    logaritmo2("./contadoresPrimos.txt")
+    logaritmo10("./contadoresPrimos.txt")
+
+    graficar("./logaritmo2.txt")
+    graficar("./logaritmo10.txt")
+
+
+def logaritmo10(archivo):
+
+    dataset = open(archivo, "r")
+
+    datos = dataset.read()
+    dataset.close()
+
+    nuevCadena = ""
+
+    datosArr = datos.split(",")
+    datosArr.pop()
+
+    for element in datosArr:
+        nuevCadena += str(math.log10(int(element)))
+        nuevCadena +=","
+
+
+    crearArchivoDeunos(nuevCadena, "./listaLogaritmo64.txt")
+
+    nuevoArchivo = open("logaritmo10.txt", "w")
+    nuevoArchivo.write(nuevCadena)
+    nuevoArchivo.close
+
+def logaritmo2(archivo):
+
+    dataset = open(archivo, "r")
+
+    datos = dataset.read()
+    dataset.close()
+
+    nuevCadena = ""
+
+    datosArr = datos.split(",")
+
+    datosArr.pop()
+
+    for element in datosArr:
+        nuevCadena += str(math.log2(int(element)))
+        nuevCadena +=","
+
+
+    crearArchivoDeunos(nuevCadena, "./listaLogaritmo64.txt")
+
+    nuevoArchivo = open("logaritmo2.txt", "w")
+    nuevoArchivo.write(nuevCadena)
+    nuevoArchivo.close
+
+
+def crearArchivoDeunos(cadena, archivoDestino):
+
+    #Creamos el archivo txt que contiene la información de los unos que tiene cada elemento del conjunto potencia
+
+    conjuntoPotencia = cadena.replace("}", "")
+    conjuntoPotencia = conjuntoPotencia.replace("{", "")
+    conjuntoPotencia = conjuntoPotencia.replace(",,", ",")
+
+    arregloBinarios = conjuntoPotencia.split(",")
+    arregloContadores = contarUnos(arregloBinarios)
     
-    if i%2==0:
-        i+=1
-    else:
-        i+=2
-#print(primos)
-primosBin = convertirACadena(primos)
-arregloContadoresBinarios = contarUnos(primosBin)
-#print(arregloContadoresBinarios)
+    cadenaContadores = ""
 
-#guardamos los arreglos con los elementos que ya tenemos guardados
-token = open("./token.txt", "w")
-token.write(str(i) + "," + str(len(primos))) #anteriormente le dimos a i el valor de k que es el token inicial, así que i es el token final
-token.close()
+    for i in range(len(arregloContadores)-1):
+        
+        cadenaContadores += str(arregloContadores[i]) + ","
 
-for j in range(cantidadPrimos, len(primos)): #Tenemos que corregir esta parte, esto queda pendiente
-    cadenaPrimos += str(primos[j]) + ","
-
-archivoPrimos = open("./primos.txt", "a")
-archivoPrimos.write(cadenaPrimos)
-archivoPrimos.close()
-
-for j in range (cantidadPrimos, len(primos)):
-    cadenaBinarios += primosBin[j] + ","
-
-archivoPrimosBinario = open("./primosBin.txt", "a")
-archivoPrimosBinario.write(cadenaBinarios)
-archivoPrimosBinario.close()
-
-for j in range (cantidadPrimos, len(primos)):
-    cadenaContadores += str(arregloContadoresBinarios[j]) + ","
-
-archivoContadores = open("./contadoresPrimos.txt", "a")
-archivoContadores.write(cadenaContadores)
-archivoContadores.close()
+    listaUnos = open(archivoDestino, "a")
+    listaUnos.write(cadenaContadores)
+    listaUnos.close()
 
 
+def graficar(archivo):
+    #Aquí se puede poner el algoritmo para plotear con vispy los unos totales.
 
-#antes de plotear, obtenemos el total de los primos que tenemos en el archivo-------------------------------------------
+    listaUnos = open(archivo, "r")
+    arregloContadores = listaUnos.read().split(",")
 
-arregloContadoresBinarios = open("./contadoresPrimos.txt", "r").read().split(",")
-arregloContadoresBinarios.pop()
+    for i in range(len((arregloContadores))-1):
+        arregloContadores[i] = float(arregloContadores[i])
 
+    arregloContadores.pop()
+    #hora de realizar el plot--------------------------------------------------------
 
-#ploteo de la grafica---------------------------------------------------------------------------------------------------
+    color = (0.3, 0.5, 0.8)
+    n_bins = 100
 
-# make the data
-'''np.random.seed(3)
-x = 4 + np.random.normal(0, 2, 24)
-y = 4 + np.random.normal(0, 2, len(x))
+    fig = vp.Fig(show=False)
+    line = fig[0:4, 0:4].plot(arregloContadores, symbol='o', width=0,
+                            face_color=color + (1,), edge_color=None,
+                            marker_size=4)
+    line.set_gl_state(depth_test=False)
+    fig.show(run=True)
 
-# size and color:
-sizes = np.random.uniform(15, 80, len(primos))
-colors = np.random.uniform(15, 80, len(arregloContadoresBinarios))
+    #fin del plot--------------------------------------------------------------------
 
-# plot
-fig, ax = plt.subplots()
+if __name__ == "__main__":
 
-ax.scatter(primos, arregloContadoresBinarios, s=sizes, c=colors, vmin=0, vmax=100)
+    bandera = "y"
 
-ax.set(xlim=(0, 8), xticks=primos,
-       ylim=(0, 8), yticks=np.arange(1, 20))
-
-plt.show()'''
-
-#otra forma de pleatar los primos--------------------------------------------------------------------------------------
-
-color = (0.3, 0.5, 0.8)
-n_bins = 100
-
-fig = vp.Fig(show=False)
-line = fig[0:4, 0:4].plot(arregloContadoresBinarios, symbol='o', width=0,
-                          face_color=color + (1,), edge_color=None,
-                          marker_size=4)
-line.set_gl_state(depth_test=False)
-fig.show(run=True)
+    while(bandera == "y"):
+        main()
+        bandera = input("calcular otro valor de n [n/y]")
