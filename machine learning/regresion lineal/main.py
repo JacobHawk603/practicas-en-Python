77d1327beec27b6e935e045c8a347012ccf29f57
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-11-17 16:41:49 -0600

    version entregable de la practica de regresion lineal

--- machine learning/regresion lineal/main.py
+++ machine learning/regresion lineal/main.py
@@ -4,23 +4,27 @@ from sklearn.model_selection import train_test_split
 
 
 def F(w, X, y):
-	return sum((w * x - y)**2 for x, y in zip(X, y))/len(y)
+	return sum((w * x - y)**2 for x, y in zip(X, y))/len(y) # <- error cuadrático medio
 
 
 def dF(w, X, y):
 	return sum(2*(w * x - y) * x for x, y in zip(X, y))/len(y)
 
 
-def print_line(points, w, iteration, line_color = None, line_style = 'dotted'):
-	list_x = []
-	list_y = []
-	for index, tuple in enumerate(points):
-		x = tuple[0]
-		y = x * w
-		list_x.append(x)
-		list_y.append(y)
-	ax1.text(x,y, iteration, horizontalalignment='right')
-	ax1.plot(list_x, list_y, color = line_color, linestyle= line_style)
+def print_line(points, w, iteration, line_color = None, line_style = 'dotted', ax = None):
+    list_x = []
+    list_y = []
+
+    if(ax == None):
+        ax = ax1
+
+    for index, tuple in enumerate(points):
+        x = tuple[0]
+        y = x * w
+        list_x.append(x)
+        list_y.append(y)
+    ax.text(x,y, iteration, horizontalalignment='right')
+    ax.plot(list_x, list_y, color = line_color, linestyle= line_style)
 
 if __name__=='__main__':
 
@@ -64,33 +68,58 @@ if __name__=='__main__':
     iterations = int(sys.argv[1])
 
     fig = plt.figure(figsize=(15, 5))
-    ax1 = fig.add_subplot(1, 2, 1)
+    ax1 = fig.add_subplot(1, 3, 1)
     ax1.set_title("Linear regression")
     ax1.set(xlabel="size", ylabel="price")
-    ax2 = fig.add_subplot(1, 2, 2)
+    ax2 = fig.add_subplot(1, 3, 2)
     ax2.set_title("Loss function")
     ax2.set(xlabel="weight", ylabel="error")
+    
+    ax3 = fig.add_subplot(1,3,3)
+    ax3.set_title("predictions")
+    ax3.set(xlabel="size", ylabel="price")
 
     ax1.scatter(X_train, Y_train)
 
     w= 0
     alpha = 0.000001
     # ~ alpha = 0.05 #Efecto similar al de no sacar el promedio
-    #aplicamos el proceso a los conjuntos de entrenamiento
+    #aplicamos el proceso a los conjuntos de entrenamiento y entrenamos el algoritmo
     for t in range(iterations):
-        error = F(w, X_train, Y_train)
-        gradient = dF(w, X_train, Y_train)
+
+        error = F(w, X_train, Y_train) #<- valor positivo
+        gradient = dF(w, X_train, Y_train) #<- valor negativo
         print ('gradient = {}'.format(gradient))
         ax2.scatter(w, error)
         ax2.text(w, error, t, horizontalalignment='right')
         list_w.append(w)
         list_error.append(error)
         
-        w = w - alpha * gradient
+        w = w - alpha * gradient #<- valor positivo
+        
         print ('iteration {}: w = {}, F(w) = {}'.format(t, w, error))
-        print_line(zip(X_train, Y_train), w, t)
+        #print_line(zip(X_train, Y_train), w, t)
             
     print_line(zip(X_train, Y_train), w, t, 'red', 'solid')
     ax2.plot(list_w, list_error, color = 'red', linestyle = 'solid')
 
+    #usamos el conjunto de prueba para predecir los valores target, teniendo el MSE ya calculado
+
+    prediccion = 0
+    for i in range (len(X_test)):
+        prediccion = (w * X_test[i])
+
+        print("real: {} \t prediccion: {}".format(str(Y_test[i]), str(prediccion)))
+
+        #calculamos el MSE de los datos de prueba
+        errorEnPrueba = F(w, X_test, Y_test)
+        print("error obtenido en la prueba: ", errorEnPrueba)
+
+    #ploteamos la lista de puntos del conjunto de prueba
+    
+    ax3.scatter(X_test, Y_test)
+    #ploteamos las rectas generadas
+
+    print_line(zip(X_test, Y_test), w, i, 'blue', 'solid', ax=ax3)
+ 
     plt.show()
