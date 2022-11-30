7f390bb1b5dfcbd8cad1eb84e6dc93c233c07c1a
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-11-29 23:37:24 -0600

    avance vision Artificial

--- vision Artificial/water-shed/librerias/filtros.py
+++ vision Artificial/water-shed/librerias/filtros.py
@@ -19,7 +19,25 @@ def kernelGauss(dimensionFiltro, sigma):
     
     return kernel
 
-def filtrarImagen(imagenBN, filtro):
+def kernelLoG(dimensionFiltro, sigma):
+
+    limiteFiltro = int((dimensionFiltro - 1)/2)
+    kernel = np.zeros((dimensionFiltro, dimensionFiltro), float)
+
+    i = 0
+    for x in range(-limiteFiltro, limiteFiltro+1):
+        j = 0
+        for y in range(-limiteFiltro, limiteFiltro+1):
+            kernel[i,j] = (1 / (2 * np.pi * sigma**4)) * (2-((x**2 + y**2)/sigma**2)) * np.exp(-((x**2 + y**2) / (2 * sigma**2))) 
+            
+            j+=1
+        i+=1
+
+    print(np.matrix(kernel))
+    
+    return kernel
+
+def filtrarImagen(imagenBNExpandida, imagenBN, filtro):
     largo, alto = imagenBN.shape
     largoFiltro, altoFiltro = filtro.shape
     filtrada = np.zeros(imagenBN.shape, imagenBN.dtype)
@@ -28,14 +46,14 @@ def filtrarImagen(imagenBN, filtro):
     x = 0
     y = 0
 
-    for i in range(largo-15):
-        for j in range(alto-15):
+    for i in range(largo):
+        for j in range(alto):
 
             x = i
             for k in range(largoFiltro):
                 y = j
                 for l in range(altoFiltro):
-                    convolucion += float(imagenBN[x,y]) * filtro[k,l]
+                    convolucion += float(imagenBNExpandida[x,y]) * filtro[k,l]
                     y += 1
             
                 x +=1
@@ -43,23 +61,48 @@ def filtrarImagen(imagenBN, filtro):
             filtrada[i,j] = int(convolucion)
             convolucion = 0
     
-    print(filtrada[:50,:50])
+    #print(filtrada[:50,:50])
 
     cv2.imshow("original", imagenBN)
-    cv2.imshow("suaizada", filtrada)
+    cv2.imshow("filtrada", filtrada)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    return filtrada
 
-def main():
-    dimensionFiltro = 15
-    sigma = 1.4
+def expandirImagen(imagenBN, filtro):
+    largo, alto =  imagenBN.shape
+    largoFiltro, altoFiltro = filtro.shape
 
-    kernel = kernelGauss(dimensionFiltro, sigma)
+    expansionFilas = alto + altoFiltro - 1
+    expansionColumnas = largo + largoFiltro - 1
+    exceso = int((largoFiltro-1)/2)
+    print(exceso)
+
+    expandida = np.zeros((expansionColumnas, expansionFilas), imagenBN.dtype)
+
+    for i in range(alto):
+        for j in range(largo):
+            #if(i> altoFiltro -1 and j> largoFiltro -1 and i<=alto and j <= largo):
+            expandida[j+exceso][i+exceso] = imagenBN[j][i]
+
+    #cv2.imshow("expandida", expandida)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+    return expandida
+
+
+'''def main():
+    dimensionFiltro = 5
+    sigma = 1
+
+    kernel = kernelLoG(dimensionFiltro, sigma)
 
     imagen = cv2.imread("../../src/lenna_main.jpg", 0)
-    filtrarImagen(imagen, kernel)
+    imagenExpandida = expandirImagen(imagen, kernel)
+    filtrarImagen(imagenExpandida, imagen, kernel)
 
 
 if __name__ == "__main__":
-    main()
+    main()'''
--- vision Artificial/water-shed/main.py
+++ vision Artificial/water-shed/main.py
@@ -1,5 +1,9 @@
 import cv2
 from librerias import binarizacion
+from librerias import filtros
+from scipy import ndimage as ndi
+from skimage.feature import peak_local_max
+import numpy as np
 
 def obtenerHistograma(imagenBN):
     contador = 0
@@ -25,16 +29,46 @@ def main():
     imagenBN = cv2.imread("../src/lenna_main.jpg", 0)
     largo, alto = imagenBN.shape
 
+    #obtenemos el histograma de la imagen para el futuro umbralado
     histograma = obtenerHistograma(imagenBN)
 
+    #aplicamos el suavizado
+    kernel = filtros.kernelLoG(5, 1)
+    
+    #obtenemos la imagen de transición
+    imagenExpandida = filtros.expandirImagen(imagenBN, kernel)
+
+    #aplicamos el filtro LoG
+    filtradaLoG = filtros.filtrarImagen(imagenExpandida, imagenBN, kernel)
+
+    #obtenemos el umbral con OTSU
     umbral = binarizacion.OTSU(histograma)
 
-    imagenUmbralada = binarizacion.umbralar(imagenBN, largo, alto, umbral)
+    #umbralamos
+    imagenUmbralada = binarizacion.umbralar(filtradaLoG, largo, alto, umbral)
+
+    #aplicamos el watershed desde la funcion
+    waterShed(imagenUmbralada)
 
     cv2.imshow("Lenna", imagen)
     cv2.imshow("Lenna blanco y negro", imagenBN)
     cv2.imshow("Lenna Umbralada", imagenUmbralada)
 
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def waterShed(imagenUmbralada):
+
+    distancia = ndi.distance_transform_edt(imagenUmbralada)
+    coordenadas = peak_local_max(min_distance=distancia, labels=imagenUmbralada)
+    mascara = np.zeros(distancia.shape, dtype=bool)
+    mascara[tuple(coordenadas.T)] = True
+    marcadores, _ = ndi.label(mascara)
+    etiquetas = cv2.watershed(imagenUmbralada, marcadores)
+
+
+    cv2.imshow("watershed", etiquetas)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
