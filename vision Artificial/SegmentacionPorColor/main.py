8dc9da80efaa4a8ed2a48beddc20f70550820a08
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-12-06 15:28:53 -0600

    asegurando los cambios

--- vision Artificial/SegmentacionPorColor/main.py
+++ vision Artificial/SegmentacionPorColor/main.py
@@ -14,21 +14,30 @@ def main():
     imagen = cv2.imread("../src/paisaje_resized.jpg")
 
     r = imagen[:,:,2]
+    g = imagen[:,:,1]
+    b = imagen[:,:,0]
     imagenBN = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
-    
+    print(imagenBN[:3,:3])
     #cv2.imshow("capa r", r)
     #cv2.waitKey(0)
     #guardamos la imagen en formato csv
-    np.savetxt("./imagen.csv", imagenBN, delimiter=',')
+    np.savetxt("./imagen.csv", r, delimiter=',')
+    np.savetxt("./imagen.csv", g, delimiter=',')
+    np.savetxt("./imagen.csv", b, delimiter=',')
 
     #ahora obtenemos el dataset a partir de la lectura del archivo csv
 
     df = pd.read_csv("./imagen.csv", sep=',', engine='python')
 
     #Le tiramos todo el dataFrame al KNN para que clusterize XD
-    kmeans.knn(df, 3)
+    dfClusters = kmeans.knn(df, 5)
 
-    print(df)
+    np.savetxt("./imagenClusterizada.csv", df, delimiter=',')
+
+    #recuperamos los clusters
+    clusters = df['centroid'].values
+
+    #print (clusters)
     
     return 0
 
