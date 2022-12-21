55314206e7d2d6a67dfcd7e093f2fc8c2fb274bb
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2022-12-21 00:20:05 -0600

    avance transformaciones2

--- vision Artificial/transformaciones/main.py
+++ vision Artificial/transformaciones/main.py
@@ -10,14 +10,47 @@ def main():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
+    rotar(imagen)
     return 0
 
 def rotar(imagen):
-    angulo = np.pi/2
+    angulo = np.pi/4
     tx = 0
-    ty = 0
+    ty = 450
 
-    matriz = [[[np.cos(angulo), -np.sin(angulo), 0], [np.sin(angulo), np.cos(angulo), 0]],[tx, ty, 1]]
+    imgTransformada = np.zeros((500,500, 3), imagen.dtype)
+
+    matriz = np.matrix([[np.cos(angulo), -np.sin(angulo), tx], [np.sin(angulo), np.cos(angulo), ty],[0, 0, 1]])
+
+    prueba = np.matrix([[1,2,3],[4,5,6],[7,8,9]])
+
+    print(matriz)
+    print(matriz.I)
+
+    #le aplcamos la transformacion a la imagen
+
+    for i in range(imagen.shape[0]):
+        for j in range(imagen.shape[1]):
+            
+            x = np.dot(matriz.I[0], [i,j,1])
+            y = np.dot(matriz.I[1], [i,j,1])
+            z = np.dot(matriz.I[2], [i,j,1])
+
+            if z != 0:
+                x /= z
+                y /= z
+
+            #print(int(x))
+            # if x >= 209:    x = 208
+            # if y>=209:      y = 208
+
+            imgTransformada[int(x),int(y)] = imagen[i,j]
+    
+
+    cv2.imshow("triangulo2", imgTransformada)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
