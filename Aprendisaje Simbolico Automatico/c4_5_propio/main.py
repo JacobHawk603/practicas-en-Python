95fd546e28c09e11ad3a365debae50cbf82d0062
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2023-10-01 20:34:44 -0600

    avance parcial, hay que particionar y hacer lo demas

--- Aprendisaje Simbolico Automatico/c4_5_propio/main.py
+++ Aprendisaje Simbolico Automatico/c4_5_propio/main.py
@@ -1,16 +1,25 @@
 from ucimlrepo import fetch_ucirepo
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def entropia(clases):
-    '''Clases -> np.ndarray que contiene los elementos por clase con los que se va a calcular la entropia'''
+def entropia(clases:np.ndarray, tamano_muestra:int=None):
+    '''Clases -> np.ndarray que contiene los elementos por clase con los que se va a calcular la entropia\n
+        (opcional) tamano_muestra -> int: tamaño de de la muestra que se proporciona, si no se especifica este atributo, se asume que los
+        elementos de las ramas conforman el total de la muestra, y con ese valor se generará la entropia de clase o de la muestra
+    '''
     i = 0   #<- Entropia, no un contador de for
-    m = 0
 
-    #calculamos el total de los elementos de cada clase
-    for clase in clases:
-        m+=clase
+    if(tamano_muestra == None):
+        m = 0
+
+        #calculamos el total de los elementos de cada clase
+        for clase in clases:
+            m+=clase
+    else:
+        m = tamano_muestra
+
 
     #calculamos la entropía
     for clase in clases:
@@ -18,30 +27,56 @@ def entropia(clases):
         if clase != 0:
             i+=(-(clase/m) * np.log2(clase/m))  #<- Importante, debe de ser logaritmo de base 2 para que de el resultado visto en clase
 
-    return i
+    return float(i)
 
-def entropia_de_Atributo(ramas):
-    '''ramas -> np.ndarray que contiene los elementos por cada decicion posible del algoritmo con los que se va a calcular la entropia'''
+def entropia_de_Atributo(ramas:np.ndarray, tamano_muestra:int=None):
+    '''ramas -> np.ndarray que contiene los elementos por cada decicion posible del algoritmo con los que se va a calcular la entropia\n
+        (opcional) tamano_muestra -> int: tamaño de de la muestra que se proporciona, si no se especifica este atributo, se asume que los
+        elementos de las ramas conforman el total de la muestra, y con ese valor se generará la entropia del atributo
+    '''
     i = 0
-    m = 0
 
-    for rama in ramas:
-        m +=rama[0] + rama[1]
+    if(tamano_muestra == None):
+        m = 0
+
+        for rama in ramas:
+            m +=rama[0] + rama[1]
+    else:
+        m = tamano_muestra
 
     for rama in ramas:
         print(rama)
         i += ((rama[0] + rama[1])/m) * entropia(rama)
 
-    return i
+    return float(i)
 
 def ganancia(entropia_general, entropia_atributo):
     return entropia_general - entropia_atributo
 
+def ganancia_aparente(entropia_general:float, entropia_atributo:float, total_elementos:int, elementos_sin_faltantes:int):
+    return float((elementos_sin_faltantes/total_elementos) * (entropia_general - entropia_atributo))
+
+
+def proporcion_de_ganancia(gan:float, i:float):
+    '''gan -> float64: ganancia del atributo o tambien la ganancia aparente, dependiendo las circunstancias\n
+        i -> float64 entropia del atributo\n\n
+
+        retorna: float64 -> proporción de ganancia para el atributo
+
+    '''
+
+    return float(gan/i)
+
+
 def ejemplo_quinlan():
     #primero realicemos una prueba de id3 con el ejemplo de quinlan para ver si vamos por buen camino
 
-    df = pd.read_excel("./ejemplo_quinlan.ods")
+    df = pd.read_excel("./ejemplo_quinlan_faltantes.ods")
     print(df)
+
+    # preprocesamos para limpiar datos nulos
+
+    df = df.fillna('missing')
 
     #separamos el corpus en X y target (y)
 
@@ -55,12 +90,12 @@ def ejemplo_quinlan():
     y_pos = 0
     y_neg = 0
 
-    print(y.values)
+    print(y.values, X['Outlook'].values)
 
-    for element in y.values:
-        if(element == 'Yes'):
+    for desicion, atribute in zip(y.values, X['Outlook'].values):
+        if(desicion == 'Yes' and atribute != 'missing'):
             y_pos += 1
-        else:
+        elif(desicion == 'No' and atribute != 'missing'):
             y_neg +=1
 
     print(y_pos, y_neg)
@@ -87,7 +122,7 @@ def ejemplo_quinlan():
             clima_LL[0] += 1
         elif element == 'Rain' and decicion == 'No':
             clima_LL[1] +=1
-    
+
     print(clima_S, clima_N, clima_LL)
 
     entropia_clima = entropia_de_Atributo([clima_S, clima_N, clima_LL])
@@ -96,21 +131,83 @@ def ejemplo_quinlan():
 
     return 0
 
-if __name__ == "__main__":
+def credit_aproval():
 
     # Vamos a comenzar por importar el dataset directamente desde python
-  
-    # # fetch dataset 
-    # credit_approval = fetch_ucirepo(id=27) 
-    
-    # # data (as pandas dataframes) 
-    # X = credit_approval.data.features 
-    # y = credit_approval.data.targets 
-    
-    # # metadata 
-    # print(credit_approval.metadata) 
-    
-    # # variable information 
-    # print(credit_approval.variables) 
 
-    ejemplo_quinlan()
+    # fetch dataset
+    credit_approval = fetch_ucirepo(id=27)
+
+    # data (as pandas dataframes)
+    X = credit_approval.data.features
+    y = credit_approval.data.targets
+
+    # Vamos llenar datos faltantes como una cadena "missing"
+    X_limpio = X.dropna()
+    y_limpio = y.dropna()
+
+    X = X.fillna('missing')
+    y = y.fillna('missing')
+
+    # metadata
+    print(credit_approval.metadata)
+
+    # variable information
+    print(credit_approval.variables)
+
+    print(X, y)
+
+    #comencemos con obtener la entropia del dataset (entropia general o entropia de la muestra)
+    contador = 0
+    y_pos = 0
+    y_neg = 0
+    for row, target in zip(X.values, y.values):
+        print(row)
+
+        hayFaltantes = False
+        for element in row:
+            if(element == 'missing'):
+                hayFaltantes = True
+
+        if(not hayFaltantes and target != 'missing'):
+            contador +=1
+
+            if(target == '+'):
+                y_pos += 1
+            elif(target == '-'):
+                y_neg += 1
+
+    print(contador, y_pos, y_neg)
+
+    M_aparente = entropia([y_pos, y_neg])   #entropia calculada con los elementos sin datos faltantes en toda la muestra
+
+    #calculemos las entropias por cada uno de los atributos de cada una de las clases
+    clases = X.columns.values
+
+    #por cada una de las clases vamos a identificar todos los tributos que contienen
+    matriz_clases = []
+    for i in range(len(clases)):
+        matriz_clases.append([clases[i]])
+        print("len({})".format(len(matriz_clases)))
+        arr_auxiliar = []
+        for element in X_limpio[clases[i]]:
+
+            en_la_matriz = False
+
+            for j in range(len(arr_auxiliar)):
+                # print(j)
+                if(arr_auxiliar[j] == element):
+                    en_la_matriz = True
+
+            if(not en_la_matriz):
+                arr_auxiliar.append(element)
+
+        matriz_clases[i].append(arr_auxiliar)
+
+    print(clases, "\n\n clases: \n\n", matriz_clases)
+
+    return 0
+
+if __name__ == "__main__":
+
+    credit_aproval()
