327ecb2095f7f75f0a7d8966dd5592b4b4022788
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2023-11-29 00:18:04 -0600

    avance monk1 OCAT

--- Aprendisaje Simbolico Automatico/OCAT/Binarizacion.py
+++ Aprendisaje Simbolico Automatico/OCAT/Binarizacion.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 from librerias_caseras.procesamiento_datasets import valores_observados
+from librerias_caseras.herramientas_listas import copiar_lista
 
-def binarizado_no_categoricos(lista_valores:list[int | float]):
+def binarizar_no_categoricos(lista_valores:list[int | float], one_hot:bool = False, num_columna:int = 1):
 
     binarizacion:list[list[int]] = []
 
@@ -11,9 +12,9 @@ def binarizado_no_categoricos(lista_valores:list[int | float]):
     observados.sort()
 
     for i, valor in enumerate(observados):
-        labels.append(f"x{i}")
+        labels.append(f"x{num_columna},{i+1}")
 
-    labels.append("valor_real")
+    labels.append(f"valor_real {num_columna}")
 
     for elemento in lista_valores:
         
@@ -21,10 +22,18 @@ def binarizado_no_categoricos(lista_valores:list[int | float]):
 
         for observado in observados:
             
-            if elemento >= observado:
-                auxiliar.append(1)
+            if one_hot:
+
+                if elemento == observado:
+                    auxiliar.append(1)
+                else:
+                    auxiliar.append(0)
+
             else:
-                auxiliar.append(0)
+                if elemento >= observado:
+                    auxiliar.append(1)
+                else:
+                    auxiliar.append(0)
 
         auxiliar.append(elemento)
         
@@ -35,10 +44,43 @@ def binarizado_no_categoricos(lista_valores:list[int | float]):
 
     return dataframe
 
+def binarizar_categoricos(lista_valores:list[str], numero_columna:int = 1):
+
+    conversion_no_categoricos:list[int] = []
+    observados = valores_observados(lista_valores)
+
+    copia_lista_valores:list[str] = copiar_lista(lista_valores)
+
+    for i, observado in enumerate(observados):
+
+        # conversion_no_categoricos.append(i)
+
+        for j, valor in enumerate(copia_lista_valores):
+            if valor == observado:
+
+                copia_lista_valores[j] = i
+
+    #ahora realizamos el proceso de binarizado no categorico para obtener el data frame
+
+    dataframe = binarizar_no_categoricos(copia_lista_valores, one_hot=True, num_columna=numero_columna)
+
+    #ahora reemplazamos la columna de valor real por los str
+
+    dataframe = dataframe.drop(f"valor_real {numero_columna}", axis=1)
+    observados_dataframe = pd.DataFrame(lista_valores, columns=[f"valor_real {numero_columna}"])
+    # print(observados_dataframe)
+    dataframe = pd.concat([dataframe, observados_dataframe], axis=1)
+
+    return dataframe
+
+
 if __name__ == "__main__":
 
     prueba:list = [0.25, 0.75, 1.00, 0.50, 0.75, 1.25, 1.75, 0.25, 1.00, 1.50, 0.50, 1.25, 1.50, 2.25, 1.75, 1.25, 2.25, 2.75, 1.25, 1.75, 2.25]
+    prueba2:list[str] = ["hola", "rojo", "azul", "verde", "rojo", "amarillo", "amarillo", "rojo", "verde"]
 
-    data = binarizado_no_categoricos(prueba)
+    data = binarizar_no_categoricos(prueba)
 
-    print(data)
+    data2 = binarizar_categoricos(prueba2)
+
+    print(data2)
---
+++ Aprendisaje Simbolico Automatico/OCAT/librerias_caseras/herramientas_listas.py
@@ -0,0 +1,10 @@
+def copiar_lista(lista:list):
+
+    copia:list = []
+
+    for elemento in lista:
+        auxiliar = elemento
+
+        copia.append(auxiliar)
+    
+    return copia
